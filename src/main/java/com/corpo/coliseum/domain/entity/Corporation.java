8a5038210a422836b952c7c5754301258338c908
Thibaut STRECKER <thibaut.strecker@gmail.com>
2021-01-29 21:24:12 +0100

    Fix/get all corporation (#30)
    * fix id : change panacheEntity to panacheBaseEntity and set uuid as id
    
    * Fix wildcard import
    
    * Fix id with generated values

--- src/main/java/com/corpo/coliseum/domain/entity/Corporation.java
+++ src/main/java/com/corpo/coliseum/domain/entity/Corporation.java
@@ -1,27 +1,40 @@
 package com.corpo.coliseum.domain.entity;
 
-import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import java.util.List;
+import java.util.UUID;
 
 @Entity
 @Table(name = "corporation")
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class Corporation extends PanacheEntity {
+public class Corporation extends PanacheEntityBase {
 
+    @Id
+    @GeneratedValue
+    public UUID id;
     @Column(unique = true)
     public String name;
     public String sport;
     @ManyToMany
+    @JoinTable(
+            name = "corporation_users",
+            joinColumns = @JoinColumn(name = "corporation_id"),
+            inverseJoinColumns = @JoinColumn(name = "corpo_user_id")
+    )
     public List<User> userList;
 }
 
